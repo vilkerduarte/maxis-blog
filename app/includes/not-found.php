a31cf3dbfa159a4351a5c2fac774e2fc0a6e93ea
Vilker Duarte <admin@du-art.tk>
2022-07-13 01:03:51 -0300

    ...

--- app/includes/not-found.php
+++ app/includes/not-found.php
@@ -1,4 +1,7 @@
 <?php
+$BODY['top'] = renderView('widgets/cabecalho',[
+  "logo"=>$sys['url_storage'].($sys['layout']['header']['logo'] == 'white' ? $sys['logo_white'] : $sys['logo'])
+]);
 $BODY['content'] = renderView('pages/not-found',array());
 if($MODAL__){
 	echo $BODY['content'];
